ec02ff152c816272cdad6fde78c745a647e099cd
mxsergeev <themaximsergeev@gmail.com>
2026-02-05 20:37:43 +0200

    final permission hell fix ?

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import { startQueryWatcher } from './services/playwright.service.js'
 
 const port = process.env.SERVER_PORT || 3333
 
+// Set umask to allow group write permissions for created files and directories
+process.umask(0o002);
+
 // Resume existing queries in the background
 if (process.env.RUN_QUEUE_WATCHER === 'true') {
   startQueryWatcher()
