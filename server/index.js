446a6583725f06d4c8fefb0f5758677018235936
mxsergeev <themaximsergeev@gmail.com>
2026-02-05 20:37:23 +0200

    start query watcher based on RUN_QUEUE_WATCHER env

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ import { startQueryWatcher } from './services/playwright.service.js'
 const port = process.env.SERVER_PORT || 3333
 
 // Resume existing queries in the background
-startQueryWatcher()
+if (process.env.RUN_QUEUE_WATCHER === 'true') {
+  startQueryWatcher()
+}
 
 app.listen(port, () => {
   console.log(`NLR-AUTODOWNLOADER listening on port ${port}`)
