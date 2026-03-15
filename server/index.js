2978a20c43aa3861da278ec7a41e2a702a74d428
mxsergeev <themaximsergeev@gmail.com>
2026-03-15 14:29:05 +0200

    Remove downloads watcher and related env variables

--- server/index.js
+++ server/index.js
@@ -7,12 +7,7 @@ import './workers/download.worker.js'
 const port = process.env.SERVER_PORT || 3333
 
 // Set umask to allow group write permissions for created files and directories
-process.umask(0o002);
-
-// Resume existing queries in the background
-if (process.env.RUN_QUEUE_WATCHER === 'true') {
-  startDownloadsWatcher()
-}
+process.umask(0o002)
 
 app.listen(port, () => {
   console.log(`NLR-AUTODOWNLOADER listening on port ${port}`)
